1419d9090b08cd61efc2de3f74c5eb79adc1a4fa
Jedrzej <jedrzej.markowski@sap.com>
2020-05-25 10:04:20 +0200

    save

--- libs/core/src/lib/time/time-column/time-column.component.ts
+++ libs/core/src/lib/time/time-column/time-column.component.ts
@@ -1,4 +1,6 @@
-import { ChangeDetectionStrategy, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, Input, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounce, debounceTime } from 'rxjs/operators';
 
 @Component({
     selector: 'fd-time-column',
@@ -6,7 +8,9 @@ import { ChangeDetectionStrategy, Component, ElementRef, Input, OnInit, ViewChil
     styleUrls: ['./time-column.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TimeColumnComponent implements OnInit {
+export class TimeColumnComponent {
+
+    readonly ELEMENT_HEIGHT: number = 48;
 
     @Input()
     rows: number[];
@@ -20,9 +24,17 @@ export class TimeColumnComponent implements OnInit {
     @ViewChild('listElement', {read: ElementRef})
     listElement: ElementRef;
 
+    @ViewChildren('timeItem')
+    timeItems: QueryList<ElementRef>;
+
+    scrolled: Subject<void> = new Subject<void>();
 
     constructor() {
         this.rows = Array.from(Array(60).keys());
+
+        this.scrolled.pipe(
+            debounceTime(20)
+        ).subscribe(() => this._handleScroll())
     }
 
     putElementUp(): void {
@@ -34,7 +46,8 @@ export class TimeColumnComponent implements OnInit {
     }
 
     handleScroll(event): void {
-        console.log(event);
+        this.scrolled.next();
+        this._throttle(this._handleScroll, 100);
     }
 
     scrollUp(): void {
@@ -49,6 +62,38 @@ export class TimeColumnComponent implements OnInit {
             top: 54,
             behavior: 'smooth'
         })
+    }
+
+    private _handleScroll(): void {
+        console.log({
+            'list': this.listElement.nativeElement.getBoundingClientRect().top,
+            'last': this.timeItems.last.nativeElement.getBoundingClientRect().top,
+            'first': this.timeItems.first.nativeElement.getBoundingClientRect().top
+        });
+
+        if (this.timeItems.first &&
+            (this.listElement.nativeElement.getBoundingClientRect().top - this.timeItems.first.nativeElement.getBoundingClientRect().top < this.ELEMENT_HEIGHT)
+        ) {
+            this.putElementUp();
+        } else if (this.timeItems.last &&
+            ( this.timeItems.last.nativeElement.getBoundingClientRect().top - this.listElement.nativeElement.getBoundingClientRect().top < this.ELEMENT_HEIGHT )
+        ) {
+            this.putElementDown();
+        }
+
+    }
+
+    private _throttle(func, timeFrame) {
+        let lastTime = 0;
+        return function () {
+            const now = new Date().getTime();
+            console.log('123');
+            if (now - lastTime >= timeFrame) {
+                console.log('redi');
+                func();
+                lastTime = now;
+            }
+        };
     }
 
 }
