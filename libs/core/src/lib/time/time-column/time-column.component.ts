008310bd8ece3def3c8312e62a2d08f0887b0bb6
Jedrzej <jedrzej.markowski@sap.com>
2020-06-15 13:48:39 +0200

    save

--- libs/core/src/lib/time/time-column/time-column.component.ts
+++ libs/core/src/lib/time/time-column/time-column.component.ts
@@ -1,6 +1,8 @@
-import { ChangeDetectionStrategy, Component, ElementRef, Input, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, Input, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { Subject } from 'rxjs';
-import { debounce, debounceTime } from 'rxjs/operators';
+import { debounceTime, tap } from 'rxjs/operators';
+
+let timeColumnUniqueId: number = 0;
 
 @Component({
     selector: 'fd-time-column',
@@ -8,9 +10,8 @@ import { debounce, debounceTime } from 'rxjs/operators';
     styleUrls: ['./time-column.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TimeColumnComponent {
-
-    readonly ELEMENT_HEIGHT: number = 48;
+export class TimeColumnComponent implements AfterViewInit {
+    readonly offset: number = 150;
 
     @Input()
     rows: number[];
@@ -21,79 +22,101 @@ export class TimeColumnComponent {
     @Input()
     active: boolean = false;
 
+    @Input()
+    id: string = 'fd-time-column-' + timeColumnUniqueId++;
+
     @ViewChild('listElement', {read: ElementRef})
     listElement: ElementRef;
 
     @ViewChildren('timeItem')
     timeItems: QueryList<ElementRef>;
 
-    scrolled: Subject<void> = new Subject<void>();
+    scrolled: Subject<any> = new Subject<any>();
+
+    currentIndicatorId: string = this.id + '-current-indicator';
 
     constructor() {
-        this.rows = Array.from(Array(60).keys());
+        this.rows = [
+            ...Array.from(Array(60).keys())
+        ];
 
         this.scrolled.pipe(
+            tap(event => event.preventDefault()),
             debounceTime(20)
-        ).subscribe(() => this._handleScroll())
+        ).subscribe(event => {
+        })
     }
 
-    putElementUp(): void {
-        this.rows.unshift(this.rows.pop());
+    ngAfterViewInit(): void {
+        const time = this.activeNumber;
+        this.activeNumber = 3;
+        this._pickTime(time);
     }
 
-    putElementDown(): void {
-        this.rows.push(this.rows.shift());
+    getId(num: number): string {
+        return this.id + '-' + num
+    }
+
+    putElementsUp(): void {
+        for (let i = 0; i < 10; i ++) {
+            this.rows.unshift(this.rows.pop());
+        }
+    }
+
+    putElementsDown(): void {
+        for (let i = 0; i < 10; i ++) {
+            this.rows.push(this.rows.shift());
+        }
     }
 
     handleScroll(event): void {
-        this.scrolled.next();
-        this._throttle(this._handleScroll, 100);
+        this.scrolled.next(event);
+    }
+
+    pick(num: number): void {
+        this._pickTime(num);
     }
 
     scrollUp(): void {
-        this.listElement.nativeElement.scrollBy({
-            top: -54,
-            behavior: 'smooth'
-        })
+        this._pickTime(this.activeNumber - 1);
     }
 
     scrollDown(): void {
-        this.listElement.nativeElement.scrollBy({
-            top: 54,
-            behavior: 'smooth'
-        })
+        this._pickTime(this.activeNumber + 1);
     }
 
     private _handleScroll(): void {
+        const listElement = this.listElement.nativeElement;
+        if (listElement.offsetHeight + listElement.scrollTop > (listElement.scrollHeight - this.offset)) {
+            this.putElementsDown();
+        } else if (listElement.scrollTop < this.offset) {
+            this.putElementsUp();
+        }
+    }
+
+    private _pickTime(num: number): void {
+        const id = this.getId(num);
+        this._scrollBy(id);
+        this.activeNumber = num;
+    }
+
+    private _scrollBy(newId: string): void {
+        const currentElement = document.getElementById(this.getId(this.activeNumber));
+        const indicator = document.getElementById(this.currentIndicatorId);
+        const newElement = document.getElementById(newId);
+
         console.log({
-            'list': this.listElement.nativeElement.getBoundingClientRect().top,
-            'last': this.timeItems.last.nativeElement.getBoundingClientRect().top,
-            'first': this.timeItems.first.nativeElement.getBoundingClientRect().top
+            currentElement: currentElement.offsetTop,
+            indicator: indicator.offsetTop,
+            newElement: newElement.offsetTop
         });
 
-        if (this.timeItems.first &&
-            (this.listElement.nativeElement.getBoundingClientRect().top - this.timeItems.first.nativeElement.getBoundingClientRect().top < this.ELEMENT_HEIGHT)
-        ) {
-            this.putElementUp();
-        } else if (this.timeItems.last &&
-            ( this.timeItems.last.nativeElement.getBoundingClientRect().top - this.listElement.nativeElement.getBoundingClientRect().top < this.ELEMENT_HEIGHT )
-        ) {
-            this.putElementDown();
-        }
+        const distance: number = newElement.offsetTop - currentElement.offsetTop;
+        this.listElement.nativeElement.scrollBy({
+            top: distance,
+            behavior: 'smooth'
+        });
 
+        this._handleScroll();
     }
-
-    private _throttle(func, timeFrame) {
-        let lastTime = 0;
-        return function () {
-            const now = new Date().getTime();
-            console.log('123');
-            if (now - lastTime >= timeFrame) {
-                console.log('redi');
-                func();
-                lastTime = now;
-            }
-        };
-    }
-
 }
