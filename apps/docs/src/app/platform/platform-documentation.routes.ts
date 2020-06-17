29cf6a5e4536ac58ec49cd63e41f80943fbb7b7e
Deepak Kumar <deepak.kumar14@sap.com>
2020-06-17 11:41:58 +0530

    docs:route fix [ci skip]

--- apps/docs/src/app/platform/platform-documentation.routes.ts
+++ apps/docs/src/app/platform/platform-documentation.routes.ts
@@ -37,6 +37,7 @@ export const ROUTES: Routes = [
                 loadChildren: () =>
                     import('./component-docs/platform-forms/platform-date-picker/platform-date-picker.module').then(
                         (m) => m.PlatformDatePickerDocsModule
+                    )
             },
             {
                 path: 'link',
@@ -79,8 +80,10 @@ export const ROUTES: Routes = [
             {
                 path: 'info-label',
                 loadChildren: () =>
-                    import('./component-docs/platform-info-label/platform-info-label-docs-module').then((m) => m.PlatformInfoLabelDocsModule)
-            },
+                    import('./component-docs/platform-info-label/platform-info-label-docs-module').then(
+                        (m) => m.PlatformInfoLabelDocsModule
+                    )
+            }
         ]
     }
 ];
